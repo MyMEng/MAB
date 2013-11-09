00315d4124c8c364cc96c18d7fe7d79cf06119cf
kacper <ks1591@bristol.ac.uk>
2013-11-09 22:56:27 +0000

    First touch

--- compile.h
+++ compile.h
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-pdflatex dissertation.tex
-bibtex dissertation.tex
-pdflatex dissertation.tex
+pdflatex dissertation
+bibtex dissertation
+pdflatex dissertation
 
 # $ latex myarticle
 # $ bibtex myarticle
